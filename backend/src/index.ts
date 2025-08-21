f5f8fccb4ea07e9d8bf55dc604896eae7c0c76ea
Fabio Rodrigues <me@fabiorodrigu.es>
2025-08-21 16:12:34 +0200

    added ok message to api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ['https://*.vercel.app'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Length', 'X-Kuma-Revision'],
