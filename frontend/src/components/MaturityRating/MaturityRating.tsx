cdf914e330a00e9efbabbdbec3cff8cb095d5b02
Marina <maryna.seidel@yahoo.de>
2025-09-25 15:21:12 +0200

    maturity rating container added

--- frontend/src/components/MaturityRating/MaturityRating.tsx
+++ frontend/src/components/MaturityRating/MaturityRating.tsx
@@ -2,7 +2,7 @@ import type { MaturityRatingProps } from './MaturityRating.types';
 import styles from './MaturityRating.module.css';
 
 const MaturityRating = ({ rating }: MaturityRatingProps) => {
-  return <div>{rating}</div>;
+  return <div className={styles.rating}>{rating}</div>;
 };
 
 export default MaturityRating;
