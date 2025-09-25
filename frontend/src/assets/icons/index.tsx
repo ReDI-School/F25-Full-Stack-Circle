3f892fe62e97cb2d434e433d0cbf87f18477b865
Giada <giada.bellan@live.it>
2025-09-25 16:19:30 +0200

    added first icon batch and created ICONS index

---
+++ frontend/src/assets/icons/index.tsx
@@ -0,0 +1,51 @@
+import accountIcon from './accountIcon.png';
+import arrowDownIcon from './arrowDownIcon.png';
+import arrowLeftIcon from './arrowLeftIcon.png';
+import arrowRightIcon from './arrowRightIcon.png';
+import arrowRightNarrowIcon from './arrowRightNarrowIcon.png';
+import arrowUpIcon from './arrowUpIcon.png';
+import circleErrorIcon from './circleErrorIcon.png';
+import crossIcon from './crossIcon.png';
+import facebookIcon from './facebookIcon.png';
+import flagIcon from './flagIcon.png';
+import infoIcon from './infoIcon.png';
+import instagramIcon from './instagramIcon.png';
+import notificationIcon from './notificationIcon.png';
+import pencilIcon from './pencilIcon.png';
+import personIcon from './personIcon.png';
+import playIcon from './playIcon.png';
+import plusThinIcon from './plusThinIcon.png';
+import plusWideIcon from './plusWideIcon.png';
+import questionIcon from './questionIcon.png';
+import searchIcon from './searchIcon.png';
+import thumbDownIcon from './thumbDownIcon.png';
+import thumbUpIcon from './thumbUpIcon.png';
+import twitterIcon from './twitterIcon.png';
+import youTubeIcon from './youTubeIcon.png';
+
+export const ICONS: Record<string, string> = {
+  account: accountIcon,
+  arrowDown: arrowDownIcon,
+  arrowLeft: arrowLeftIcon,
+  arrowRight: arrowRightIcon,
+  arrowRightNarrow: arrowRightNarrowIcon,
+  arrowUp: arrowUpIcon,
+  circleError: circleErrorIcon,
+  close: crossIcon,
+  facebook: facebookIcon,
+  flag: flagIcon,
+  info: infoIcon,
+  instagram: instagramIcon,
+  notification: notificationIcon,
+  pencil: pencilIcon,
+  person: personIcon,
+  play: playIcon,
+  plusThin: plusThinIcon,
+  plusWide: plusWideIcon,
+  question: questionIcon,
+  search: searchIcon,
+  thumbDown: thumbDownIcon,
+  thumbUp: thumbUpIcon,
+  twitter: twitterIcon,
+  youTube: youTubeIcon,
+};
