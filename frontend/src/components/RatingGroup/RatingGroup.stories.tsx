975ee16b1225e27310e80d837ba95fe3af73cd3d
Christheo Guipo <christheo.guipo@gmail.com>
2025-09-29 12:18:26 +0200

    feat: add stories for all size variants and add keys to children

--- frontend/src/components/RatingGroup/RatingGroup.stories.tsx
+++ frontend/src/components/RatingGroup/RatingGroup.stories.tsx
@@ -12,12 +12,35 @@ export default meta;
 
 type Story = StoryObj<typeof RatingGroup>;
 
-export const Default: Story = {
+export const Small: Story = {
   args: {
     children: [
-      <IconButton icon="thumbs-down" variant="outlined" theme="primary" selected/>,
-      <IconButton icon="thumbs-up" variant="outlined" theme="primary"  selected={false} />,
-      <IconButton icon="double-thumbs-up" variant="outlined" theme="primary" selected={false} />,
+      <IconButton key={0} icon="thumbs-down" size='small' variant="outlined" theme="primary" selected />,
+      <IconButton key={1} icon="thumbs-up" size='small' variant="outlined" theme="primary" selected={false} />,
+      <IconButton key={2} icon="double-thumbs-up" size='small' variant="outlined" theme="primary" selected={false} />,
     ],
+    size: 'small'
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    children: [
+      <IconButton key={0} icon="thumbs-down" size='medium' variant="outlined" theme="primary" selected />,
+      <IconButton key={1} icon="thumbs-up" size='medium' variant="outlined" theme="primary" selected={false} />,
+      <IconButton key={2} icon="double-thumbs-up" size='medium' variant="outlined" theme="primary" selected={false} />,
+    ],
+    size: 'medium'
+  },
+};
+
+export const Large: Story = {
+  args: {
+    children: [
+      <IconButton key={0} icon="thumbs-down" size='large' variant="outlined" theme="primary" selected />,
+      <IconButton key={1} icon="thumbs-up" size='large' variant="outlined" theme="primary" selected={false} />,
+      <IconButton key={2} icon="double-thumbs-up" size='large' variant="outlined" theme="primary" selected={false} />,
+    ],
+    size: 'large'
   },
 };
