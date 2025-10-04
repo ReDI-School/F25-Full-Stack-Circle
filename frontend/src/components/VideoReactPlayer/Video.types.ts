dd1e3c64c5e264b11ac251c14173475a8f283cc9
Christheo Guipo <christheo.guipo@gmail.com>
2025-10-04 20:43:32 +0200

    feat: create VideoProps

--- frontend/src/components/VideoReactPlayer/Video.types.ts
+++ frontend/src/components/VideoReactPlayer/Video.types.ts
@@ -1,15 +1,60 @@
-
 interface VideoProps {
+  /**
+   * The url or path of a video
+   */
   src: string;
+
+  /**
+   * Set to true or false to play or pause the media
+   */
   playing?: boolean;
-  light: boolean | string;
-  playIcon: React.ReactElement;
-  width?: number;
-  height?: number;
-  className?: string;
-  onPlay?: () => void;
-  onPause?: () => void;
+
+  /**
+   * Set to true or false to loop the media
+   * Default: false
+   */
+  loop?: boolean;
+
+  /**
+   * Set the volume of the player, between 0 and 1
+   * ◦ null uses default volume on all players
+   */
   volume?: number;
+
+  /**
+   * Set the playback rate of the player
+   * Default: 1
+   */
+  playbackRate?: number | undefined;
+
+  /**
+   * Set the width of the player
+   * Default: 320px
+   */
+  width?: string;
+
+  /**
+   * Set the height of the player
+   * Default: 180px
+   */
+  height?: string;
+
+  /**
+   * Set to true to show just the video thumbnail, which loads the full player on click
+   * ◦ Pass in an image URL to override the preview image
+   * Default: false
+   */
+  light?: boolean | string;
+
+  /**
+   * Element or component to use as the play icon in light mode
+   */
+  playIcon?: React.ReactElement;
+
+  /**
+   * Sets the current time
+   */
+  currentTime?: number;
 }
 
 export type { VideoProps };
