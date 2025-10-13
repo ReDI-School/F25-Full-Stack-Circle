2203b8a53170992de57d65fe4e537df029b7668a
MJ <ahmad_almujawer@icloud.com>
2025-10-13 20:16:14 +0200

    Refactor RoutePaths: Update routePaths and routeLabels objects

--- frontend/src/config/RoutePaths.ts
+++ frontend/src/config/RoutePaths.ts
@@ -1,15 +1,7 @@
 export const routePaths = {
-  home: () => '/',
-  shows: () => '/shows',
-  news: () => '/news',
-  myList: () => '/list',
-  language: () => '/language',
-};
-
-export const routeLabels: Record<keyof typeof routePaths, string> = {
-  home: 'Home',
-  shows: 'TV Shows',
-  news: 'News & Popular',
-  myList: 'My List',
-  language: 'Browse By Language',
+  home: () => ['Home', '/'],
+  shows: () => ['TV Shows', '/shows'],
+  news: () => ['News & Popular', '/news'],
+  myList: () => ['My List', '/list'],
+  language: () => ['Browse By Language', '/language'],
 };
