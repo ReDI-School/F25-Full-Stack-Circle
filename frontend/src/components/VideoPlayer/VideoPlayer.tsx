084436884282b414a383e456b74c232648a92f73
Christheo Guipo <christheo.guipo@gmail.com>
2025-10-16 18:50:10 +0200

    feat: add functionalities for: - hiding mouse and control bar on full screen. - most events test: test controlBar with  progress bar and control icons

--- frontend/src/components/VideoPlayer/VideoPlayer.tsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,8 +1,21 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import type { VideoPlayerProps } from './VideoPlayer.types';
 import ReactPlayer from 'react-player';
 import { cva } from 'class-variance-authority';
 import styles from './VideoPlayer.module.css';
+import { VideoControlBar } from '../VideoControlBar';
+import { ProgressBar } from '../ProgressBar';
+import PlayIcon from '../../assets/icons/playIcon.svg?react';
+// import MuteVolume from '../../assets/icons/muteVolume.svg?react';
+// import MiddleVolume from '../../assets/icons/middleVolume.svg?react';
+import HighVolume from '../../assets/icons/highVolume.svg?react';
+import PlayBack from '../../assets/icons/playback.svg?react';
+import Next from '../../assets/icons/next.svg?react';
+import Rewind from '../../assets/icons/rewind.svg?react';
+import Fullscreen from '../../assets/icons/fullscreen.svg?react';
+import Forward from '../../assets/icons/forward.svg?react';
+import EpisodeList from '../../assets/icons/episodeList.svg?react';
+import Caption from '../../assets/icons/caption.svg?react';
 
 const styledWrapper = cva(styles.videoPlayerWrapper, {
   variants: {
@@ -11,6 +24,9 @@ const styledWrapper = cva(styles.videoPlayerWrapper, {
       medium: styles.medium,
       large: styles.large,
       full: styles.full,
+    },
+    showMouseInFullscreen: {
+      false: styles.hideMouse,
     },
   },
 });
@@ -29,24 +45,28 @@ const Video = ({
   fullscreen = false,
 }: VideoPlayerProps) => {
   const playerRef = useRef<HTMLVideoElement | null>(null);
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
 
-  const [state, setState] = useState({
+  const initialState = {
+    playing: playing,
+    currentTime: currentTime,
+    loaded: 0,
+    duration: 0,
+    start: false,
+    fullscreen: fullscreen,
+    showControls: true,
     light: light,
-    // will add more states here
-  });
-
-  const handleOnClickPreview = () => {
-    setState((prev) => ({ ...prev, light: false }));
   };
 
-  const handleOnStart = (e: React.SyntheticEvent<HTMLVideoElement, Event>) => {
-    console.log('onStart', e);
-    if (playerRef.current && fullscreen) {
-      playerRef.current
-        .requestFullscreen()
-        .then()
-        .catch((e) => console.log(e));
-    }
+  const [state, setState] = useState(initialState);
+
+  const handleOnClickPreview = () => {
+    setState((prev) => ({ ...prev, light: false, playing: true }));
+  };
+
+  const handleOnStart = () => {
+    console.log('OnStart');
+    setState((prev) => ({ ...prev, start: true }));
   };
 
   const handleOnPlay = () => {
@@ -60,7 +80,7 @@ const Video = ({
   const handleOnReady = () => {
     console.log('OnReady');
     if (playerRef.current) {
-      playerRef.current.currentTime = currentTime;
+      playerRef.current.currentTime = state.currentTime;
     }
   };
 
@@ -81,41 +101,169 @@ const Video = ({
   };
 
   const handleOnEnded = () => {
-    console.log('onEnded');
-    setState((prev) => ({ ...prev, light: light }));
+    const player = playerRef.current;
+
+    setState((prev) => {
+      if (player) {
+        if (document.fullscreenElement != null) document.exitFullscreen();
+        return { ...initialState, duration: player.duration, playing: false };
+      } else return { ...prev };
+    });
   };
 
   const handleOnError = () => {
     console.log('onError');
   };
 
-  const handleOnTimeUpdate = () => {};
+  const handleOnTimeUpdate = () => {
+    console.log('OnTimeUpdate');
+    const player = playerRef.current;
+
+    setState((prev) => {
+      if (player) {
+        return { ...prev, currentTime: player.currentTime };
+      } else {
+        return { ...prev };
+      }
+    });
+  };
 
   const handleOnProgress = () => {
-    console.log('onProgress');
+    console.log('OnHandleProgress');
+    const player = playerRef.current;
+
+    setState((prev) => {
+      if (player) {
+        return { ...prev, loaded: player.buffered.end(player.buffered.length - 1) };
+      } else {
+        return { ...prev };
+      }
+    });
   };
 
   const handleOnDurationChange = () => {
-    console.log('onDurationChange');
+    console.log('OnDurationChange');
+    const player = playerRef.current;
+
+    setState((prev) => {
+      if (player) {
+        return { ...prev, duration: player.duration };
+      } else {
+        return { ...prev };
+      }
+    });
   };
 
+  const handleOnClickPlay = () => {
+    setState((prev) => ({ ...prev, playing: !prev.playing }));
+  };
+
+  const handleOnClickRewind = () => {
+    const player = playerRef.current;
+    setState((prev) => {
+      const newTime = Math.max(0, prev.currentTime - 10);
+      if (player) player.currentTime = newTime;
+      return { ...prev, currentTime: newTime };
+    });
+  };
+
+  const handleOnClickForward = () => {
+    const player = playerRef.current;
+    setState((prev) => {
+      const newTime = Math.min(prev.duration, prev.currentTime + 10);
+      if (player) player.currentTime = newTime;
+      return { ...prev, currentTime: newTime };
+    });
+  };
+
+  const handleSetNewValue = (newValue: number) => {
+    const player = playerRef.current;
+
+    setState((prev) => {
+      if (player) {
+        player.currentTime = newValue;
+        return { ...prev, currentTime: newValue };
+      } else {
+        return { ...prev };
+      }
+    });
+  };
+
+  const handleOnClickFullscreen = () => {
+    const wrapper = wrapperRef.current;
+    if (state.fullscreen) {
+      document.exitFullscreen();
+    } else {
+      if (wrapper) wrapper.requestFullscreen();
+    }
+  };
+
+  useEffect(() => {
+    const handleWrapperOnFullscreenChange = () => {
+      const wrapper = wrapperRef.current;
+
+      if (document.fullscreenElement === null) {
+        setState((prev) => ({ ...prev, fullscreen: false, showControls: true }));
+      } else if (document.fullscreenElement === wrapper) {
+        setState((prev) => ({ ...prev, fullscreen: true, showControls: true }));
+      }
+    };
+
+    document.addEventListener('fullscreenchange', handleWrapperOnFullscreenChange);
+
+    return () => {
+      document.removeEventListener('fullscreenchange', handleWrapperOnFullscreenChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    const wrapper = wrapperRef.current;
+    let timeout: ReturnType<typeof setTimeout>;
+
+    if (!wrapper || !state.fullscreen) return;
+
+    const handleWrapperOnMouseMove = () => {
+      clearTimeout(timeout);
+      setState((prev) => ({ ...prev, showControls: true }));
+
+      timeout = setTimeout(() => {
+        setState((prev) => ({ ...prev, showControls: false }));
+      }, 3000);
+    };
+
+    wrapper.addEventListener('mousemove', handleWrapperOnMouseMove);
+
+    return () => {
+      wrapper.removeEventListener('mousemove', handleWrapperOnMouseMove);
+
+      clearTimeout(timeout);
+    };
+  }, [state.fullscreen, state.showControls]);
+
   return (
-    <div className={styledWrapper({ size })}>
+    <div
+      ref={wrapperRef}
+      className={styledWrapper({ size, showMouseInFullscreen: state.showControls })}
+    >
       <ReactPlayer
         ref={playerRef}
         style={{ aspectRatio: '16/9' }}
         width={width}
         height={height}
         src={src}
-        playing={playing}
+        playing={state.playing}
         light={state.light}
         playIcon={playIcon}
         volume={volume}
         playbackRate={playbackRate}
+        preload="auto"
+        controls={false}
         config={{
           vimeo: {
             byline: false,
             portrait: false,
+            controls: false,
+            fullscreen: fullscreen,
           },
         }}
         // Called when user clicks the light mode preview
@@ -123,7 +271,7 @@ const Video = ({
         // Called when media is loaded and ready to play.
         onReady={handleOnReady}
         // Called when media starts playing
-        onStart={(e) => handleOnStart(e)}
+        onStart={handleOnStart}
         // Called when the playing prop is set to true
         onPlay={handleOnPlay}
         // Called when media actually starts playing
@@ -147,6 +295,37 @@ const Video = ({
         // Callback containing duration of the media, in seconds
         onDurationChange={handleOnDurationChange}
       />
+      {!state.light && state.showControls && (
+        <VideoControlBar>
+          <div style={{ backgroundColor: 'transparent', height: 1 }} />
+          <ProgressBar
+            value={state.currentTime}
+            loaded={state.loaded}
+            duration={state.duration}
+            setNewValue={(newValue: number) => handleSetNewValue(newValue)}
+            showThumb
+            color="red"
+          />
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              gap: '1rem',
+            }}
+          >
+            <PlayIcon width={20} height={20} onClick={handleOnClickPlay} />
+            <Rewind width={20} height={20} onClick={handleOnClickRewind} />
+            <Forward width={20} height={20} onClick={handleOnClickForward} />
+            <HighVolume width={20} height={20} />
+            <p>Title Placeholder S1:Ep1 Episode Title</p>
+            <Next width={20} height={20} />
+            <EpisodeList width={20} height={20} />
+            <Caption width={20} height={20} />
+            <PlayBack width={20} height={20} />
+            <Fullscreen width={20} height={20} onClick={handleOnClickFullscreen} />
+          </div>
+        </VideoControlBar>
+      )}
     </div>
   );
 };
