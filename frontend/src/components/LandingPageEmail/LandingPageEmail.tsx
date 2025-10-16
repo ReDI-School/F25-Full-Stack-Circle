a817e093364cc792b03e6db7a7bd0f726e75d42d
Marina <maryna.seidel@yahoo.de>
2025-10-16 17:33:31 +0200

    add changes after PR review

--- frontend/src/components/LandingPageEmail/LandingPageEmail.tsx
+++ frontend/src/components/LandingPageEmail/LandingPageEmail.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import styles from './LandingPageEmail.module.css';
 import { Button } from '../Button';
 import InputField from '../InputField';
@@ -11,34 +12,19 @@ const LandingPageEmail = () => {
   };
 
   return (
-    <>
-      <div className={styles.container}>
-        <h1 className={styles.textL}>Unlimited movies, TV shows, and more</h1>
-        <h4 className={styles.textM}>Watch anywhere. Cancel anytime.</h4>
-
-        <h5 className={styles.textS}>
-          Ready to watch? Enter your email to create or restart your membership.
-        </h5>
-        <div className={styles.email}>
-          <InputField
-            size="Medium"
-            type="Email"
-            state="Default"
-            value={email}
-            onChange={setEmail}
-          />
-          <Button
-            variant="primary"
-            size="large"
-            icon="chevron"
-            iconPosition="after"
-            onClick={handleGetStarted}
-          >
-            Get Started
-          </Button>
-        </div>
+    <div className={styles.container}>
+      <h1 className={styles.textL}>Unlimited movies, TV shows, and more</h1>
+      <h4 className={styles.textM}>Watch anywhere. Cancel anytime.</h4>
+      <h5 className={styles.textS}>
+        Ready to watch? Enter your email to create or restart your membership.
+      </h5>
+      <div className={styles.email}>
+        <InputField size="Large" value={email} onChange={setEmail} />
+        <Button size="large" icon="chevron" onClick={handleGetStarted}>
+          Get Started
+        </Button>
       </div>
-    </>
+    </div>
   );
 };
 
