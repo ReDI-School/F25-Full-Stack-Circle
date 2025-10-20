3c7fc3807968c177c3f99737a011553b69bd1c7e
MJ <ahmad_almujawer@icloud.com>
2025-10-20 16:41:24 +0200

    Added types and render users to WhoIsWatching page

--- frontend/src/pages/Profiles/WhoIsWatching.tsx
+++ frontend/src/pages/Profiles/WhoIsWatching.tsx
@@ -1,12 +1,30 @@
+import styles from './WhoIsWatching.module.css';
+
 const WhoIsWatching = () => {
-  const users = ['Ahmad', 'Koubrse', 'Chikas'];
+  interface User {
+    id: string;
+    name: string;
+  }
+  const users: User[] = [
+    { id: '1', name: 'Ahmad' },
+    { id: '2', name: 'Koubrse' },
+    { id: '3', name: 'Chikas' },
+  ];
 
   return (
-    <div>
-      <h1>Who's watching?</h1>
-      <ul>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Who's watching?</h1>
+      <ul className={styles.list}>
         {users.map((user) => (
-          <li>{user}</li>
+          <li key={user.id} className={styles.item}>
+            {/* Use button component ↓ */}
+            <button
+              className={styles.profileBtn}
+              onClick={() => console.log('Selected user', user.name)}
+            ></button>
+
+            <span className={styles.name}>{user.name}</span>
+          </li>
         ))}
       </ul>
     </div>
