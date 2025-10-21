8b4035285ecd3d8b13e18b2ada90752030c8a223
Dmytro Yukhymuk <dima.yukhimuk@gmail.com>
2025-10-21 22:50:05 +0200

    Refactor `routePaths` and `NavigationMenu`

---
+++ frontend/src/routes/routePaths.ts
@@ -0,0 +1,9 @@
+export const routePaths = {
+  home: () => ({ label: 'Home', path: '/' }),
+  shows: () => ({ label: 'TV Shows', path: '/shows' }),
+  news: () => ({ label: 'News & Popular', path: '/news' }),
+  myList: () => ({ label: 'My List', path: '/list' }),
+  language: () => ({ label: 'Browse By Language', path: '/language' }),
+  signIn: () => ({ label: 'Sign In', path: '/signin' }),
+  signUp: () => ({ label: 'Sign Up', path: '/signup' }),
+};
