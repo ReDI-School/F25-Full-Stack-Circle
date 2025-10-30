ff08eaca361520d5ebc0d37e5dffb7a9ab898620
MJ <ahmad_almujawer@icloud.com>
2025-10-30 20:56:49 +0100

    Update users API and hooks to handle array responses

--- frontend/src/config/usersAPI.ts
+++ frontend/src/config/usersAPI.ts
@@ -4,12 +4,12 @@ import type { User } from './user.types';
 
 const api = axios.create({ baseURL: 'http://localhost:4000/' });
 
-const usersData = (res: { data: User }) => res.data;
+const usersData = (res: { data: User[] }) => res.data;
 
 export const userAPI = {
-  getAll: () => api.get<User>('/users').then(usersData),
-  getOne: (id: string) => api.get<User>(`users${id}`).then(usersData),
-  create: (data: User) => api.post<User>(`/users`, data).then(usersData),
-  update: (id: string, data: User) => api.put<User>(`/users${id}`, data).then(usersData),
+  getAll: () => api.get<User[]>('/users').then(usersData),
+  getOne: (id: string) => api.get<User[]>(`users${id}`).then(usersData),
+  create: (data: User) => api.post<User[]>(`/users`, data).then(usersData),
+  update: (id: string, data: User) => api.put<User[]>(`/users${id}`, data).then(usersData),
   delete: (id: string) => api.delete(`/users/id${id}`).then(usersData),
 };
