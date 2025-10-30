bf95f4a77a3f8bfb519903a8c403a4d31e75c7da
MJ <ahmad_almujawer@icloud.com>
2025-10-30 20:57:07 +0100

    Refactor Home component to use useUsers hook

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,77 +1,34 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import logo from '../../assets/images/logo.svg';
 import Button from '../../components/Button/Button';
-import { useConfig } from '../../hooks';
 import { NavigationMenu } from '../../components/NavigationMenu';
+import { useUsers } from '../../hooks/useUsers';
+import type { User } from '../../config/user.types';
 
 import styles from './Home.module.css';
 
-interface User {
-  name: string;
-  email: string;
-}
-
 const Home = () => {
-  const [users, setUsers] = useState<User[]>([
-    {
-      name: 'Fabio',
-      email: 'test@fabio.com',
-    },
-    {
-      name: 'Perial',
-      email: 'dperial44@gmail.com',
-    },
-    {
-      name: 'Christheo',
-      email: 'christheo.guipo@gmail.com',
-    },
-    {
-      name: 'Marina',
-      email: 'maryna.seidel@yahoo.de',
-    },
-    {
-      name: 'Lyubomir',
-      email: 'lakovski@gmail.com',
-    },
-    // add your name
-  ]);
-  const [loadingUsers, setLoadingUsers] = useState<boolean>(false);
-  const { config, loadingConfig } = useConfig();
-
+  const { users, loading, error } = useUsers();
   const navigate = useNavigate();
 
   const handleSignIn = () => {
     navigate('/signin');
   };
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      if (loadingConfig || loadingUsers) return;
-
-      const response = await fetch(`${config?.apiUrl}/users`);
-      const data = await response.json();
-      setUsers(data.users);
-      setLoadingUsers(false);
-    };
-
-    fetchUsers();
-  }, [config, loadingConfig, loadingUsers]);
-
   return (
     <div className={styles.home}>
       <NavigationMenu />
       <img src={logo} alt="Rediflix Logo" width={500} />
       <div>
-        {loadingUsers ? (
-          <div>Redi team</div>
-        ) : (
+        {loading && <div>Loading users...</div>}
+        {error && <div>Error fetching users</div>}
+        {!loading && !error && (
           <>
-            {users.length > 0 ? (
-              users.map((user) => <div key={user.name}>{user.email}</div>)
+            {users?.length ? (
+              users.map((user: User) => <div key={user.id || user.name}>{user.email}</div>)
             ) : (
-              <div>Finally</div>
+              <div>No users found</div>
             )}
           </>
         )}
