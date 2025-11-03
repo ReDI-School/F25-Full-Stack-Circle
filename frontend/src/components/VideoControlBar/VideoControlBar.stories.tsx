c18b6853d6fcfb49d4b3f6c12c150694d12b97f9
Christheo Guipo <christheo.guipo@gmail.com>
2025-11-03 17:50:09 +0100

    feat: modify styling and create stories

--- frontend/src/components/VideoControlBar/VideoControlBar.stories.tsx
+++ frontend/src/components/VideoControlBar/VideoControlBar.stories.tsx
@@ -1,10 +1,7 @@
 import type { Meta, StoryFn } from '@storybook/react-vite';
-import VideoControlBar from './VideoControlBar';
-import { ProgressBar, type ProgressBarProps } from '../ProgressBar';
-import type { VideoControlBarProps } from './VideoControlBar.types';
-import { useState } from 'react';
-// import { Button } from '../Button';
-
+import { VideoControlBar } from './index';
+import type { VideoControlBarProps } from './index';
+import { useRef, useState } from 'react';
 
 const meta: Meta<VideoControlBarProps> = {
   title: 'Components/VideoControlBar',
@@ -14,38 +11,103 @@ const meta: Meta<VideoControlBarProps> = {
 
 export default meta;
 
-const Template: StoryFn<ProgressBarProps> = (args: ProgressBarProps) => {
-  const [value, setValue] = useState(args.value);
-  const handleNewValue = (newValue: number) => {
-    setValue(newValue);
+const Template: StoryFn<VideoControlBarProps> = (args: VideoControlBarProps) => {
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const [isPlaying, setIsPlaying] = useState(args.isPlaying);
+
+  const [currentTime, setCurrentTime] = useState(args.progressBarProps.value);
+  const handleCurrentTimeChange = (newTime: number) => {
+    setCurrentTime(newTime);
+  };
+
+  const [volume, setVolume] = useState(args.volumeSliderProps.value);
+  const handleVolumeChange = (newValue: number) => {
+    setVolume(newValue);
+  };
+
+  const handleOnPlayButtonClick = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  const handleOnRewindButtonClick = () => {
+    setCurrentTime((prev) => prev - 10);
+  };
+
+  const handleOnForwardButtonClick = () => {
+    setCurrentTime((prev) => prev + 10);
+  };
+
+  const handleOnPlaybackButtonClick = () => {
+    alert('Displays the Playback component (not yet implemented).');
+  };
+
+  const handleOnCaptionButtonClick = () => {
+    alert('Displays the Caption component (not yet implemented).');
+  };
+
+  const handleOnNextButtonClick = () => {
+    alert('Displays the Next Episode component (not yet merged).');
+  };
+
+  const handleOnEpisodeListButtonClick = () => {
+    alert('Displays the Episode List component (not yet merged).');
+  };
+
+  const handleOnFullscreenButtonClick = () => {
+    const wrapper = wrapperRef.current;
+    if (wrapper) {
+      if (isFullscreen) {
+        document.exitFullscreen();
+        setIsFullscreen(false);
+      } else {
+        wrapper.requestFullscreen();
+        setIsFullscreen(true);
+      }
+    }
   };
 
   return (
-    <div style={{ position: 'relative', width: 800, height: 400 }}>
-      <VideoControlBar>
-        <ProgressBar
-          {...args}
-          value={value}
-          setNewValue={(newValue: number) => handleNewValue(newValue)}
-          duration={args.duration}
-          loaded={args.loaded}
-          showThumb={args.showThumb}
-        />
-        <div>
-          {/* <Button type="button" icon={"play"} iconOnly variant='outlined' /> */}
-          {/* <button style={{backgroundColor: 'transparent', borderStyle: 'none'  }} type='button'>
-            <PlayIcon color='white' />
-          </button> */}
-        </div>
-      </VideoControlBar>
+    <div ref={wrapperRef} style={{ position: 'relative', width: '100%', height: 400 }}>
+      <VideoControlBar
+        title={args.title}
+        isPlaying={isPlaying}
+        progressBarProps={{
+          ...args.progressBarProps,
+          value: currentTime,
+          setNewValue: handleCurrentTimeChange,
+        }}
+        volumeSliderProps={{
+          ...args.volumeSliderProps,
+          value: volume,
+          onChange: handleVolumeChange,
+          orientation: 'horizontal'
+        }}
+        onPlayButtonClick={handleOnPlayButtonClick}
+        onCaptionButtonClick={handleOnCaptionButtonClick}
+        onRewindButtonClick={handleOnRewindButtonClick}
+        onForwardButtonClick={handleOnForwardButtonClick}
+        onPlaybackButtonClick={handleOnPlaybackButtonClick}
+        onNextButtonClick={handleOnNextButtonClick}
+        onEpisodeListButtonClick={handleOnEpisodeListButtonClick}
+        onFullscreenButtonClick={handleOnFullscreenButtonClick}
+      />
     </div>
   );
 };
 
 export const Default = Template.bind({});
 Default.args = {
-  value: 60,
-  showThumb: true,
-  loaded: 80,
-  duration: 200,
+  title: 'Sanctuary E1 Episode 1',
+  isPlaying: true,
+  volumeSliderProps: {
+    value: 20,
+  },
+  progressBarProps: {
+    value: 63,
+    showThumb: true,
+    loaded: 80,
+    duration: 4000,
+  },
 };
