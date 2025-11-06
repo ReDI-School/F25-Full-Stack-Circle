8b5d5bfe10aa0048bc4d7cbd03a54d18e0847382
Nikita <191010.n@gmail.com>
2025-11-06 14:18:01 +0100

    Fixed some issues

---
+++ frontend/src/api/video/videoApi.types.ts
@@ -0,0 +1,10 @@
+interface VideoData {
+  name?: string;
+  duration?: number;
+  url?: string;
+  movie_title_id?: number;
+  season_id?: number;
+  episode_number?: number;
+}
+
+export type { VideoData };
