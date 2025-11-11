3730bec10f6082b103551ae905616b02991f6f71
Christheo Guipo <christheo.guipo@gmail.com>
2025-11-11 19:52:57 +0100

    feat: create title route and include titleRouter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import userRouter from './routes/userRoutes';
+import titleRouter from './routes/titleRoutes';
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.use(
 
 // Routes
 app.use('/users', userRouter);
+app.use('/titles', titleRouter);
 
 // Health check
 app.get('/', (req: Request, res: Response) => {
