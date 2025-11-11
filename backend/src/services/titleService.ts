db635f6b9e016073dddfb18f49389a1272cdb53c
Christheo Guipo <christheo.guipo@gmail.com>
2025-11-11 19:55:00 +0100

    bug: error in creating title

---
+++ backend/src/services/titleService.ts
@@ -0,0 +1,51 @@
+import { Category, Prisma, Season, Video, VideoType } from 'generated/prisma';
+import prisma from 'src/libs/prisma';
+
+export class TitleService {
+  async getAllTitles() {
+    return await prisma.title.findMany({ include: { category: true, season: true, video: true } });
+  }
+
+  async getTitleById(id: number) {
+    const title = await prisma.title.findUnique({
+      where: { id },
+    });
+
+    // include all relationship and remove if not needed
+    if (title?.type === VideoType.MOVIE)
+      return await prisma.title.findUnique({
+        where: { id },
+        include: { category: true, video: true },
+      });
+    else if (title?.type === VideoType.SERIES)
+      return await prisma.title.findUnique({
+        where: { id },
+        include: { category: true, season: true },
+      });
+    else
+      return await prisma.title.findUnique({
+        where: { id },
+        include: { category: true, season: true, video: true },
+      });
+  }
+
+  // async createTitle(title: Prisma.TitleCreateInput) {
+  async createTitle(title: {
+    id: number;
+    name: string;
+    type: VideoType;
+    category?: Category[];
+    season?: Season[];
+    video?: Video;
+  }) {
+    return await prisma.title.create({ data: title });
+  }
+
+  async updateTitle(id: number, title: Prisma.TitleUpdateInput) {
+    return await prisma.title.update({ where: { id }, data: title });
+  }
+
+  async deleteTitle(id: number) {
+    return await prisma.title.delete({ where: { id } });
+  }
+}
