2db3ac275398fa53b572c743adbd06bb64804f00
Christheo Guipo <christheo.guipo@gmail.com>
2025-11-12 21:35:56 +0100

    refactor: map title based on its type

--- backend/src/services/titleService.ts
+++ backend/src/services/titleService.ts
@@ -6,36 +6,47 @@ export class TitleService {
     return await prisma.title.findMany({ include: { category: true, season: true, video: true } });
   }
 
-  async getTitleById(id: number) {
+  async getTitleByIdIncludeAll(id: number) {
     const title = await prisma.title.findUnique({
       where: { id },
+      include: { category: true, season: true, video: true },
     });
 
-    // include all relationship and remove if not needed
-    if (title?.type === VideoType.MOVIE)
-      return await prisma.title.findUnique({
-        where: { id },
-        include: { category: true, video: true },
-      });
-    else if (title?.type === VideoType.SERIES)
-      return await prisma.title.findUnique({
-        where: { id },
-        include: { category: true, season: true },
-      });
-    else
-      return await prisma.title.findUnique({
-        where: { id },
-        include: { category: true, season: true, video: true },
-      });
+    return title;
   }
 
-  // async createTitle(title: Prisma.TitleCreateInput) {
+  async getTitleById(id: number) {
+    const title = await this.getTitleByIdIncludeAll(id);
+    if (!title) return;
+
+    const { name, type, category, season, video, createdAt, updatedAt } = title;
+
+    const titleTypeMovie = { id, name, type, category, video, createdAt, updatedAt };
+    const titleTypeSeries = { id, name, type, category, season, createdAt, updatedAt };
+
+    switch (title.type) {
+      case VideoType.MOVIE:
+        return titleTypeMovie;
+      case VideoType.SERIES:
+        return titleTypeSeries;
+      default:
+        return title;
+    }
+  }
+
   async createTitle(title: Prisma.TitleCreateInput) {
-    return await prisma.title.create({ data: title });
+    return await prisma.title.create({
+      data: title,
+      include: { category: true, season: true, video: true },
+    });
   }
 
   async updateTitle(id: number, title: Prisma.TitleUpdateInput) {
-    return await prisma.title.update({ where: { id }, data: title });
+    return await prisma.title.update({
+      where: { id },
+      data: title,
+      include: { category: true, season: true, video: true },
+    });
   }
 
   async deleteTitle(id: number) {
