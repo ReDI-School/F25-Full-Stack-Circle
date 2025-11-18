065ce12a326af58efaf199f82f97f464dc33fb1e
Dmytro Yukhymuk <dima.yukhimuk@gmail.com>
2025-11-18 21:59:34 +0100

    Unify `Header`; Refactor `Navigation` & routes

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,34 +1,55 @@
+import { Link } from 'react-router';
+
+import SmallAvatarIcon from '../../assets/icons/smallAvatar.svg';
 import RediflixLogo from '../../assets/images/logo.svg';
 import { languagesMap } from '../../constants/languages';
-import Button from '../Button/Button';
+import { Button } from '../Button';
 import { NavigationMenu } from '../NavigationMenu';
-import Select from '../Select/Select';
+import { Select } from '../Select';
 import type { HeaderProps } from './Header.types';
 
+import { navigationItems, routePaths } from '../../routes/routePaths';
+
 import styles from './Header.module.css';
+
+const selectOptions = Object.entries(languagesMap).map(([value, label]) => ({
+  value,
+  label,
+}));
+const defaultLanguage = selectOptions.find((option) => option.value === 'en');
 
 export const Header = ({ type }: HeaderProps) => {
   const isPrivate = type === 'private';
   const isPublic = type === 'public';
-
-  const selectOptions = Object.entries(languagesMap).map(([value, label]) => ({
-    value,
-    label,
-  }));
 
   return (
     <header className={styles.header}>
       <img src={RediflixLogo} alt="Rediflix Logo" className={styles.logo} />
       {isPrivate ? (
         <>
-          <NavigationMenu />
-          {/* // TODO: add search, notifications, profile icon */}
+          <NavigationMenu className={styles.navigation} navItems={navigationItems} />
+          <div className={styles.iconButtons}>
+            <Button className={styles.iconButton} aria-label="Search" icon="search" iconOnly />
+            <Button
+              className={styles.iconButton}
+              aria-label="Notifications"
+              icon="notification"
+              iconOnly
+            />
+          </div>
+          <div className={styles.userProfile}>
+            <Button className={styles.profileButton} aria-label="User menu" icon="arrowDown">
+              <img src={SmallAvatarIcon} alt="User profile" className={styles.avatar} />
+            </Button>
+          </div>
         </>
       ) : (
         isPublic && (
           <div className={styles.selectors}>
-            <Select options={selectOptions} placeholder="English" />
-            <Button onClick={() => {}}>Sign In</Button>
+            <Select options={selectOptions} selected={defaultLanguage} />
+            <Link to={routePaths.signIn().path} className={styles.signInLink}>
+              Sign In
+            </Link>
           </div>
         )
       )}
