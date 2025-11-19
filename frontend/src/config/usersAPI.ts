af30a445ecfa0f922eff4e90a74261055fcab362
Ahmad AlMujawer <78796559+ahmad-mj@users.noreply.github.com>
2025-11-19 21:11:45 +0100

    Update usersAPI
    Co-authored-by: Dima Yukhymuk <dima.yukhimuk@gmail.com>

--- frontend/src/config/usersAPI.ts
+++ frontend/src/config/usersAPI.ts
@@ -8,7 +8,7 @@ const usersData = (res: { data: User[] }) => res.data;
 
 export const userAPI = {
   getAll: () => api.get<User[]>('/users').then(usersData),
-  getOne: (id: string) => api.get<User[]>(`users${id}`).then(usersData),
+  getOne: (id: string) => api.get<User[]>(`/users${id}`).then(usersData),
   create: (data: User) => api.post<User[]>(`/users`, data).then(usersData),
   update: (id: string, data: User) => api.put<User[]>(`/users${id}`, data).then(usersData),
   delete: (id: string) => api.delete(`/users/id${id}`).then(usersData),
