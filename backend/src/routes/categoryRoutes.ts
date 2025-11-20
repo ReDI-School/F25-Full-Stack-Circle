edb562dc976df77ef97c0182e12d80a94c899a0f
Shimmerpeace <peacedamian1@gmail.com>
2025-11-20 18:36:24 +0100

    updated

--- backend/src/routes/categoryRoutes.ts
+++ backend/src/routes/categoryRoutes.ts
@@ -4,19 +4,11 @@ import { CategoryController } from '../controllers/categoryController';
 const categoryController = new CategoryController();
 const categoryRouter = Router();
 
-// GET /category
+
 categoryRouter.get('/', (req, res) => categoryController.getAllCategories(req, res));
-
-// GET /category/:id
 categoryRouter.get('/:id', (req, res) => categoryController.getCategoryById(req, res));
-
-// POST /category
 categoryRouter.post('/', (req, res) => categoryController.createCategory(req, res));
-
-// PUT /category/:id
 categoryRouter.put('/:id', (req, res) => categoryController.updateCategory(req, res));
-
-// DELETE /category/:id
 categoryRouter.delete('/:id', (req, res) => categoryController.deleteCategory(req, res));
 
 export default categoryRouter;
