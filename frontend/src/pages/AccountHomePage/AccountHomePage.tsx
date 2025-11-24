d16288bd6caff6d5c2dfe42409ab011d70131454
Nikita <191010.n@gmail.com>
2025-11-24 17:00:44 +0100

    finished the 1st section

--- frontend/src/pages/AccountHomePage/AccountHomePage.tsx
+++ frontend/src/pages/AccountHomePage/AccountHomePage.tsx
@@ -1,5 +1,6 @@
-import { Button } from '../../components';
+import { Button, MaturityRating } from '../../components';
 import Logo from '../../assets/icons/netflix-logo.svg?react';
+import Replay from '../../assets/icons/replay-icon.svg?react';
 import heroUrl from '../../assets/images/hero.webp';
 
 import styles from './AccountHomePage.module.css';
@@ -12,27 +13,37 @@ const AccountHomePage = () => {
         backgroundImage: `url(${heroUrl})`,
       }}
     >
-      <div className={styles.wrapper}>
-        <div className={styles.mainText}>
-          <div className={styles.logoWrapper}>
-            <Logo />
-            <span>Series</span>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.mainText}>
+            <div className={styles.logoWrapper}>
+              <Logo />
+              <span>Series</span>
+            </div>
+            <h1 className={styles.mainHeader}>
+              House
+              <br />
+              of
+              <br />
+              ninjas
+            </h1>
           </div>
-          <h1 className={styles.mainHeader}>
-            House
-            <br />
-            of
-            <br />
-            ninjas
-          </h1>
+          <p>
+            Years after retiring from their formidable ninja lives, a dysfunctional family must
+            return to shadowy missions to counteract a string of looming threats.
+          </p>
+          <div className={styles.buttonsWrapper}>
+            <Button children="Play" variant="white" icon="playBlack" iconPosition="before" />
+            <Button children="More Info" variant="secondary" icon="info" iconPosition="before" />
+          </div>
         </div>
-        <p className={styles.desc}>
-          Years after retiring from their formidable ninja lives, a dysfunctional family must return
-          to shadowy missions to counteract a string of looming threats.
-        </p>
-        <div className={styles.buttonsWrapper}>
-          <Button children="Play" variant="white" icon="playBlack" iconPosition="before" />
-          <Button children="More Info" variant="secondary" icon="info" iconPosition="before" />
+      </div>
+      <div className={styles.ratingWrapper}>
+        <a href="#" className={styles.replay}>
+          <Replay />
+        </a>
+        <div className={styles.rating}>
+          <MaturityRating rating="TV-14" />
         </div>
       </div>
     </section>
